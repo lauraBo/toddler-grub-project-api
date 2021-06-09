58cd846c257a027e1884d84b822ffc9b32592ab7
LBo <BodellLaura@gmail.com>
2021-06-09 12:14:13 -0400

    added actions to recipes controller and private method, added validations to recipe model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,34 @@
 class RecipesController < ApplicationController
   def index
+    recipes = Recipe.order("created_at DESC")
+    render json: recipes
   end
 
   def create
+    recipe = Recipe.create(recipe_param)
+    render json: recipe
   end
 
   def update
+    recipe = Recipe.find(params[:id])
+    recipe.update_attributes(recipe_param)
+    render json: recipe
   end
 
   def show
+    recipe = Recipe.find(params[:id])
+    render json: recipe 
   end
 
   def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    head :no_content, status: :ok
   end
+
+  private
+    def recipe_param
+      params.require(:recipe).permit(:title, :ingredients, :method)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  get 'recipes/create'
-  get 'recipes/update'
-  get 'recipes/show'
-  get 'recipes/destroy'
-  root 'homepage#index' #get 'homepage/index'  
+  root 'homepage#index' #get 'homepage/index' ?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
