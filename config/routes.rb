f068afa23147e4138b2bc4104de586e9a80b6f0e
LBo <BodellLaura@gmail.com>
2021-06-09 12:34:42 -0400

    defined new routes in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
-Rails.application.routes.draw do
-  root 'homepage#index' #get 'homepage/index' ?
+#Rails.application.routes.draw do
+  #root 'homepage#index' #get 'homepage/index' ?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#end
+
+
+Rails.application.routes.draw do
+  scope '/api/v1' do
+    resources :recipes
+  end
 end
