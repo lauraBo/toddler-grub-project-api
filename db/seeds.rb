c5f34231791dc7fc4040d42e9bc696b8401b9932
LBo <BodellLaura@gmail.com>
2021-06-09 12:28:04 -0400

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+9.times do |i|
+    Recipe.create(
+    title: "Recipe #{i + 1}",
+    ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+    method: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat.',
+    time: '30',
+    freezable: 'no'
+)
+  end
